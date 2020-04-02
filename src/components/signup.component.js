3462897130f3d563e8d506ca25083f96348aae6a
stangalla <stangalla@miraclesoft.com>
2020-04-02 13:44:28 -0400

    first commit

---
+++ src/components/signup.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from "./node_modules/react";
+
+export default class SignUp extends Component {
+    render() {
+        return (
+            <div>
+                <h3>React SignUp Component</h3>
+            </div>
+        );
+    }
+}
